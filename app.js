2da6cab603ee516110458e9a8b264a9261100b3c
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-06-28 18:00:07 +0100

    set-up: build RESTful APIs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const books = require('./routes/api/books')
+
 const app = express();
 
 connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+app.use('/api/books', books)
 
 const port = process.env.PORT || 8082;
 
